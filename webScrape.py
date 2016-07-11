fb1ce378ff252a919c4f604e176392f3bac50d32
Scott Manifold <shm.qed@gmail.com>
2016-07-10 18:14:38 -0700

    Added basic selenium functionality to webScrape. Built in .gitignore

--- webScrape.py
+++ webScrape.py
@@ -1,11 +1,20 @@
-import urllib.request
+from sys import version_info
+if version_info[0] < 3:
+    import urllib2 as urequest
+else:
+    import urllib.request as urequest
+
 from bs4 import BeautifulSoup
-import requests
 import dbConnect
 
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#=============================================================================
 def makeSoup(url):
-    html = urllib.request.urlopen(url).read()
+    html = urequest.urlopen(url).read()
     soup = BeautifulSoup(html,'lxml')
     return soup
 
@@ -14,20 +23,27 @@ def makeSoup(url):
 # Parse Logs (parsely.io)
 #=============================================================================
 
-baseURL = "http://parsely.io"
+parseBaseURL = "http://parsely.io"
 leaderboard = "http://parsely.io/parser/leaderboard/all/all/1500000/the-harbinger/live/0/"
 
 def getPlayerLinks(leaderboard):
     soup = makeSoup(leaderboard)
     leaderTable = soup.find("tbody").find_all('tr')
-    playerPages = [baseURL + "/" + tr.a["href"] for tr in leaderTable]
+    playerPages = [parseBaseURL + "/" + tr.a["href"] for tr in leaderTable]
     return playerPages
 
 # This portion still doesn't work
 def scrapeLogData(playerPage):
-    urlTest = playerPage + '#log'
-    test = requests.get(urlTest ,headers={'referer': urlTest.replace("#log","")}).content
-    soup = makeSoup(test)
+    driver = webdriver.Chrome()
+    driver.get(playerPage)
+    elem = driver.find_element_by_xpath("//ul[@class='nav nav-tabs']/li[a/@href='#log']")
+    elem.click()
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//div[@class='table-responsive log']"))
+    )
+    html = driver.page_source
+    driver.close()
+    soup = BeautifulSoup(html,'lxml')
     logView = soup.find("div",id= "log")
     tableData = logView.find("tbody").find_all("tr")
     for row in tableData:
@@ -36,7 +52,7 @@ def scrapeLogData(playerPage):
 #=============================================================================
 # Ability Database (TOR Community)
 #=============================================================================
-baseURL = "https://torcommunity.com"
+dbBaseURL = "https://torcommunity.com"
 abilityList = "https://torcommunity.com/database/search/ability?fil=1:&filop=0&filval=204&filsub=0&"
 
 def getAbilityPage(abilityList):
@@ -46,7 +62,7 @@ def getAbilityPage(abilityList):
         tableData = currentPage.find("div", class_="database_wrapper")
         rowData = tableData.find_all("div", style="display:inline;")
         for rd in rowData:
-            abilityLinks.append(baseURL + "/" + rd.a['href'])
+            abilityLinks.append(dbBaseURL + "/" + rd.a['href'])
 
         nextPage = tableData.find("a", string = "Next")
         if type(nextPage) == type(None):
